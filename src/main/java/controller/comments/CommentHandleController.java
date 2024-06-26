41d8fad6c83db808cb8421a5b0a4b61d1247ce87
inhoo <a52169434@gmail.com>
2024-06-26 16:46:26 +0900

    댓글기능
    댓글기능을 만들었다

---
+++ src/main/java/controller/comments/CommentHandleController.java
@@ -0,0 +1,46 @@
+package controller.comments;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.dao.CommentDao;
+import model.vo.Comment;
+import model.vo.User;
+
+@WebServlet("/submit-comment")
+public class CommentHandleController extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		User authUser = (User) request.getSession().getAttribute("authUser");
+		try {
+
+			String body = request.getParameter("body");
+			String password = request.getParameter("password");
+			int postNo = Integer.parseInt(request.getParameter("postNo"));
+			String writerId = authUser.getId();
+			CommentDao commentDao = new CommentDao();
+			Comment one = new Comment();
+			one.setBody(body);
+			one.setWritedAt(new Date(System.currentTimeMillis()));
+			one.setWriterId(writerId);
+			one.setPassword(password);
+			one.setPostNo(postNo);
+			boolean r = commentDao.save(one);
+			List<Comment> comments = commentDao.findAllByPostNo(postNo);
+            request.setAttribute("comments", comments);
+			if (r) {
+				response.sendRedirect(request.getContextPath() + "/posts/view?no=" + postNo);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/main/java/model/dao/CommentDao.java
+++ src/main/java/model/dao/CommentDao.java
@@ -2,60 +2,97 @@ package model.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.vo.Comment;
 import oracle.jdbc.datasource.impl.OracleDataSource;
 
+
+
 public class CommentDao {
-	//=========================저장===========================================================================================
-		public boolean save(Comment newComment) throws Exception {
-			OracleDataSource ods = new OracleDataSource();
-			ods.setURL("jdbc:oracle:thin:@//3.36.66.249:1521/xe");
-			ods.setUser("community_inside");
-			ods.setPassword("oracle");
+	// =========================저장===========================================================================================
+	public boolean save(Comment newComment) throws Exception {
+		OracleDataSource ods = new OracleDataSource();
+		ods.setURL("jdbc:oracle:thin:@//3.36.66.249:1521/xe");
+		ods.setUser("community_inside");
+		ods.setPassword("oracle");
 
-			try (Connection conn = ods.getConnection()) {
+		try (Connection conn = ods.getConnection()) {
 
-				PreparedStatement stmt = conn.prepareStatement("INSERT INTO USERS VALUES(?,?,?,?,?,?,?,?,?)");
-				stmt.setInt(1, newComment.getNo());
-				stmt.setString(2, newComment.getBody());
-				stmt.setDate(3, newComment.getWritedAt());
-				stmt.setInt(4, newComment.getLikes());
-				stmt.setInt(5, newComment.getDislikes());
-				stmt.setString(6, newComment.getWriterId());
-				stmt.setString(7, newComment.getPassword());
+			PreparedStatement stmt = conn.prepareStatement("INSERT INTO COMMENTS VALUES(COMMENTS_SEQ.NEXTVAL,?,?,?,?,?,?,?)");
+			stmt.setString(1, newComment.getBody());
+			stmt.setDate(2, newComment.getWritedAt());
+			stmt.setInt(3, newComment.getLikes());
+			stmt.setInt(4, newComment.getDislikes());
+			stmt.setString(5, newComment.getWriterId());
+			stmt.setString(6, newComment.getPassword());
+			stmt.setInt(7, newComment.getPostNo());
+
+			int r = stmt.executeUpdate();
+			return r == 1 ? true : false;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+
+	}
+
+	// =================================NO로찾기=========================================
+	public List<Comment> findAllByPostNo(int no) throws SQLException {
+		OracleDataSource ods = new OracleDataSource();
+		ods.setURL("jdbc:oracle:thin:@//3.36.66.249:1521/xe");
+		ods.setUser("community_inside");
+		ods.setPassword("oracle");
+		try (Connection conn = ods.getConnection()) {
 			
-
-				int r = stmt.executeUpdate();
-				return r == 1 ? true : false;
-			} catch (Exception e) {
-				e.printStackTrace();
-				return false;
-			}
-
+			PreparedStatement stmt = conn.prepareStatement("SELECT * FROM COMMENTS WHERE POST_NO=?");
+			stmt.setInt(1, no);
+			ResultSet rs = stmt.executeQuery();
+			List<Comment> comments = new ArrayList<>();
+			while (rs.next()) {
+				Comment one = new Comment(rs.getInt("NO"),rs.getString("BODY"),rs.getDate("WRITED_AT"),rs.getInt("LIKES"),rs.getInt("DISLIKES")
+						,rs.getString("WRITER_ID"),rs.getString("PASSWORD"),rs.getInt("POST_NO"));
+				comments.add(one);
+			} 
+			
+			return comments;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
 		}
-	//========================삭제================================================================================================
-		
-		public boolean deleteByNo(String password) throws SQLException {
-			OracleDataSource ods = new OracleDataSource();
-			ods.setURL("jdbc:oracle:thin:@//3.36.66.249:1521/xe");
-			ods.setUser("community_inside");
-			ods.setPassword("oracle");
+	}//가나다순
+	
 
-			try (Connection conn = ods.getConnection()) {
+	// ========================삭제================================================================================================
 
-				PreparedStatement stmt = conn.prepareStatement("DELETE FROM COMMENTS WHERE PASSWORD=?");
-				stmt.setString(1, password);
+	public boolean deleteByPassword(String password) throws SQLException {
+		OracleDataSource ods = new OracleDataSource();
+		ods.setURL("jdbc:oracle:thin:@//3.36.66.249:1521/xe");
+		ods.setUser("community_inside");
+		ods.setPassword("oracle");
 
-				int r = stmt.executeUpdate();
+		try (Connection conn = ods.getConnection()) {
 
-				return r == 1 ? true : false;
-			} catch (Exception e) {
-				System.out.println(e);
-				return false;
-			}
+			PreparedStatement stmt = conn.prepareStatement("DELETE FROM COMMENTS WHERE Password LIKE ?");
+			stmt.setString(1, password);
 
+			int r = stmt.executeUpdate();
+
+			return r == 1 ? true : false;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
 		}
-	//======================================================================	
+
+	}
+	// ======================================================================
+	
+	
+	
+	
+	
+	
 }
