e635005e1364532134af3bd3d795b87638df8b7c
ghm00 <gudals2917@naver.com>
2024-06-27 16:42:58 +0900

    fix:로그아웃

--- src/main/java/controller/LogoutController.java
+++ src/main/java/controller/LogoutController.java
@@ -9,13 +9,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/logout")
-public class LogoutController extends HttpServlet{
+public class LogoutController extends HttpServlet {
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if (request.getSession().getAttribute("authUser") == null) {
-			response.sendRedirect(request.getContextPath() + "/index");
-			return;
-		}
-		request.getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.getSession().invalidate();
+		response.sendRedirect(request.getContextPath()+"/index");
 	}
-	}
+}
